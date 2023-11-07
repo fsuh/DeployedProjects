80a1953e03d2370e228d796183b98f336f7681b0
fsuh <92304761+fsuh@users.noreply.github.com>
2023-11-07 10:37:35 +0200

    updated hero text

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -8,10 +8,10 @@ const Hero = () => {
 						Fuh Suh's <span>Projects</span>
 					</h1>
 					<p>
-						These are the list of deployed projects I have worked with, on my
+						These are a list of deployed projects I have worked with, on my
 						journey of learning web development. I have many other projects in
-						my github but those were not deployed. This CMS will be updated as I
-						development more software web projects
+						my github but those yet to be fine-tuned and deployed. This CMS will
+						be updated as I develope more software web projects
 					</p>
 				</div>
 				<div className="img-container">
